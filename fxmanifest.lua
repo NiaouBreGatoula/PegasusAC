10ee9cb101f4cfb4f6465c47aeb96cc5277260c4
NiaouBreGatoula <67291791+NiaouBreGatoula@users.noreply.github.com>
2023-10-17 23:27:46 +0300

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -11,27 +11,3 @@ author 'Pegasus Team - https://discord.io/PegasusAC'
 description 'PegasusAC'
 version '0.2'
 
-server_scripts {
-	'@mysql-async/lib/MySQL.lua',
-	'server/sv_config.lua',
-	'server/*',
-}
-
-client_scripts {
-	'client/*',
-}
-
-ui_page 'client/html/index.html'
-
-
-files {
-	'client/html/index.html',
-    'client/html/js/*',
-	'client/html/css/*',
-	'client/html/yeet.ogg',
-}
-
-dependencies {
-	'screenshot-basic'
-}
-
