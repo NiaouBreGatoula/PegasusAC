a697780ffb890be64b3d6df2c972a864e0df2147
George Mos <67291791+NiaouBreGatoula@users.noreply.github.com>
2025-02-13 01:23:50 +0200

    Create version.js

---
+++ version.js
@@ -0,0 +1,4 @@
+{
+  "version": "2.4",
+  "changelog": "Update the ban system and whitelist system to json file. Update event protection to secure and work it better with players. Update heartbeat system / fixing the bug of anticheat stop detection. Added some more detections such as antiaimbot system and antistatebag crash"
+}
