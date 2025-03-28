145f9e6c81535e8b263c6ae151c0aabc5fa728e3
George Mos <georgemos940@gmail.com>
2025-03-28 20:05:15 +0200

    Update version.js

--- version.js
+++ version.js
@@ -1,4 +1,4 @@
 {
-  "version": "2.4",
-  "changelog": "Update the ban system and whitelist system to json file. Update event protection to secure and work it better with players.  Added some more detections such as antiaimbot system and antistatebag crash"
+  "version": "2.5",
+  "changelog": "Major anticheat rework with better performance and stability. Fixed Susano bypasses (noclip, freecam, etc.), DUITextures, godmode detection, screenshot bug, and false punishments. New backdoor detection (JavaScript), revamped punishment system, improved installer, and updated event protection (FXAP). Added new admin menu, config options (e.g., disable prints, privacy), support for QBus/QBox/ESX, MaxSpoofScore detection, and setup mode. Fixed memory leak (now ~1MB usage), honeypots, staffnearby, and admin-deleted weapons. Created data folder for JSONs and improved Discord logs."
 }
