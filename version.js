f61d103d5d4cf1064c992a3409c75d08df393ace
George Mos <georgemos940@gmail.com>
2025-03-31 19:07:59 +0300

    Update version.js

--- version.js
+++ version.js
@@ -1,4 +1,4 @@
 {
-  "version": "2.5",
+  "version": "2.51",
   "changelog": "Major anticheat rework with better performance and stability. Fixed Susano bypasses (noclip, freecam, etc.), DUITextures, godmode detection, screenshot bug, and false punishments. New backdoor detection (JavaScript), revamped punishment system, improved installer, and updated event protection (FXAP). Added new admin menu, config options (e.g., disable prints, privacy), support for QBus/QBox/ESX, MaxSpoofScore detection, and setup mode. Fixed memory leak (now ~1MB usage), honeypots, staffnearby, and admin-deleted weapons. Created data folder for JSONs and improved Discord logs."
 }
